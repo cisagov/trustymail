ff8124c1bb050341a8495d0292b60d3cb22b263c
h-m-f-t <cameron@hmft.xyz>
2017-08-11 16:00:58 -0400

    line cleanup

--- trustymail/trustymail.py
+++ trustymail/trustymail.py
@@ -16,7 +16,8 @@ CSV_HEADERS = [
     "MX Record", "Mail Servers",
     "SPF Record", "Valid SPF", "SPF Results",
     "DMARC Record", "Valid DMARC", "DMARC Results",
-    "DMARC Record on Base Domain", "Valid DMARC Record on Base Domain", "DMARC Results on Base Domain", "DMARC Policy",
+    "DMARC Record on Base Domain", "Valid DMARC Record on Base Domain",
+    "DMARC Results on Base Domain", "DMARC Policy",
     "Syntax Errors"
 ]
 
