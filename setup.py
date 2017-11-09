4814b8905593bd47112c24b35638ee8ef177abba
h-m-f-t <cameron.dixon@beta.dhs.gov>
2017-11-09 00:41:07 -0800

    Pip plumbing

--- setup.py
+++ setup.py
@@ -1,34 +1,70 @@
 """
-Setup module for trustymail
+setup module for trustymail
 
 Based on:
 
-- https://github.com/dhs-ncats/pshtt
+- https://packaging.python.org/distributing/
+- https://github.com/pypa/sampleproject/blob/master/setup.py
 """
 
 from setuptools import setup
+from trustymail import __version__
 
 setup(
+    name='trustymail',
 
-        name='trustymail',
-        version='0.0.1',
-        description="Scan DNS records for best mail practices.",
+    version=__version__,
+    description='Scan domains and return data based on trustworthy email best practices',
 
-        download_url="https://github.com/dhs-ncats/trustymail",
+    # NCATS "homepage"
+    url='https://www.dhs.gov/cyber-incident-response',
+    # The project's main homepage
+    download_url='https://github.com/dhs-ncats/trustymail',
 
-        packages=['trustymail'],
+    # Author details
+    author='Department of Homeland Security, National Cybersecurity Assessments and Technical Services team',
+    author_email='ncats@hq.dhs.gov',
 
-        install_requires=[
-            'requests',
-            'docopt',
-            'py3dns',
-            'pyspf==2.0.11',
-            'publicsuffix'
-        ],
+    license='License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
 
-        entry_points={
-            'console_scripts': [
-                'trustymail = trustymail.cli:main'
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='email authentication, STARTTLS',
+
+    packages=['trustymail'],
+
+    install_requires=[
+        'requests',
+        'docopt',
+        'publicsuffix'
+        'py3dns',
+        'pyspf==2.0.11',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'trustymail = trustymail.cli:main'
             ]
         }
 )
