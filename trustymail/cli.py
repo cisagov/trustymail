ba9d60c2b03da97a7a013b0c58e8cf87bd75c1f6
Shane Frasier <jeremy.frasier@beta.dhs.gov>
2017-11-22 15:05:28 -0500

    Changed double quotes to single quotes for consistency

--- trustymail/cli.py
+++ trustymail/cli.py
@@ -13,7 +13,7 @@ Options:
                               servers.  (Default is the FQDN of the host from
                               which trustymail is being run.)
   --smtp-ports=PORTS          A comma-delimited list of ports at which to look
-                              for SMTP servers.  (Default is "25,465,587".)
+                              for SMTP servers.  (Default is '25,465,587'.)
   --no-smtp-cache             Do not cache SMTP results during the run.  This
                               may results in slower scans due to testing the
                               same mail servers multiple times.
@@ -51,85 +51,85 @@ _DEFAULT_SMTP_PORTS = {25, 465, 587}
 def main():
     args = docopt.docopt(__doc__, version=__version__)
 
-    if args["--debug"]:
+    if args['--debug']:
         logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
     # Allow for user to input a csv for many domain names.
-    if args["INPUT"][0].endswith(".csv"):
-        domains = trustymail.domain_list_from_csv(open(args["INPUT"][0]))
+    if args['INPUT'][0].endswith('.csv'):
+        domains = trustymail.domain_list_from_csv(open(args['INPUT'][0]))
     else:
-        domains = args["INPUT"]
+        domains = args['INPUT']
 
-    if args["--timeout"] is not None:
-        timeout = int(args["--timeout"])
+    if args['--timeout'] is not None:
+        timeout = int(args['--timeout'])
     else:
         timeout = 5
 
-    if args["--smtp-timeout"] is not None:
-        smtp_timeout = int(args["--smtp-timeout"])
+    if args['--smtp-timeout'] is not None:
+        smtp_timeout = int(args['--smtp-timeout'])
     else:
         smtp_timeout = 5
 
-    if args["--smtp-localhost"] is not None:
-        smtp_localhost = args["--smtp-localhost"]
+    if args['--smtp-localhost'] is not None:
+        smtp_localhost = args['--smtp-localhost']
     else:
         smtp_localhost = None
 
-    if args["--smtp-ports"] is not None:
+    if args['--smtp-ports'] is not None:
         smtp_ports = {int(port) for port in args['--smtp-ports'].split(',')}
     else:
         smtp_ports = _DEFAULT_SMTP_PORTS
 
-    if args["--dns"] is not None:
+    if args['--dns'] is not None:
         dns_hostnames = args['--dns'].split(',')
     else:
         dns_hostnames = None
 
     # --starttls implies --mx
-    if args["--starttls"]:
-        args["--mx"] = True
+    if args['--starttls']:
+        args['--mx'] = True
 
     # User might not want every scan performed.
     scan_types = {
-                    "mx": args["--mx"],
-                    "starttls": args["--starttls"],
-                    "spf": args["--spf"],
-                    "dmarc": args["--dmarc"]
+                    'mx': args['--mx'],
+                    'starttls': args['--starttls'],
+                    'spf': args['--spf'],
+                    'dmarc': args['--dmarc']
                  }
 
     domain_scans = []
     for domain_name in domains:
         domain_scans.append(trustymail.scan(domain_name, timeout,
                                             smtp_timeout, smtp_localhost,
-                                            smtp_ports, not args["--no-smtp-cache"],
+                                            smtp_ports, not args['--no-smtp-cache'],
                                             scan_types, dns_hostnames))
 
     # Default output file name is results.
-    if args["--output"] is None:
-        output_file_name = "results"
+    if args['--output'] is None:
+        output_file_name = 'results'
     else:
-        output_file_name = args["--output"]
+        output_file_name = args['--output']
 
     # Ensure file extension is present in filename.
-    if args["--json"] and ".json" not in output_file_name:
-        output_file_name += ".json"
-    elif ".csv" not in output_file_name:
-        output_file_name += ".csv"
+    if args['--json'] and '.json' not in output_file_name:
+        output_file_name += '.json'
+    elif '.csv' not in output_file_name:
+        output_file_name += '.csv'
 
-    if args["--json"]:
+    if args['--json']:
         json_out = trustymail.generate_json(domain_scans)
-        if args["--output"] is None:
+        if args['--output'] is None:
             print(json_out)
         else:
             write(json_out, output_file_name)
-            logging.warn("Wrote results to %s." % output_file_name)
+            logging.warn('Wrote results to %s.' % output_file_name)
     else:
         trustymail.generate_csv(domain_scans, output_file_name)
 
 
 def write(content, out_file):
     parent = os.path.dirname(out_file)
-    if parent is not "":
+    if parent is not '':
         mkdir_p(parent)
 
     f = open(out_file, 'w')  # no utf-8 in python 2
