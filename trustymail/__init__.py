e54a0354c215dae8560f66368844e3a16e775bef
Shane Frasier <jeremy.frasier@beta.dhs.gov>
2018-04-06 16:11:13 -0400

    Bumping patch version

--- trustymail/__init__.py
+++ trustymail/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals, absolute_import, print_function
 
-__version__ = '0.5.4-dev'
+__version__ = '0.5.5'
 
 PublicSuffixListFilename = 'public_suffix_list.dat'
 PublicSuffixListReadOnly = False
