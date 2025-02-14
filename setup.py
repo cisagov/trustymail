6900b5ec560272dcde858cec9b39a855e5dfc5ec
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-02-14 12:38:33 -0500

    Revert "Remove support for Python 3.7"
    This reverts commit d50245a6b5ea7746a12aa99d8b6fa0e3a3175833.
    
    This project must still support Python 3.7.

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
