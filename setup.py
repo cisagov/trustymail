3339f081ba536437760966e0ad5a9af4de187929
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-02-14 12:37:56 -0500

    Revert "Remove support for Python 3.8"
    This reverts commit 9ee2ce916440416a27690e7453971e267724d5d0.
    
    This project must still support Python 3.8.

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -82,7 +83,7 @@ setup(
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     # What does your project relate to?
     keywords="email authentication STARTTLS",
     packages=find_packages(where="src"),
